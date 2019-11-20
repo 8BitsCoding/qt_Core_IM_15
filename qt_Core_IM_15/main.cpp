7688571226e61303687049b2c94f84f0fff85eca
kimtaehyung <taehyeong77@naver.com>
2019-11-20 13:38:48 +0900

    초기 커밋.

---
+++ qt_Core_IM_15/main.cpp
@@ -0,0 +1,40 @@
+#include <QtCore/QCoreApplication>
+
+#include <qdir.h>
+#include <qfileinfo.h>
+#include <qdatetime.h>
+#include <qdebug.h>
+
+void list(QString path) {
+	qInfo() << "Listing: " << path;
+
+	QDir dir(path);
+	qInfo() << dir.absolutePath();
+
+	QFileInfoList dirs = dir.entryInfoList(QDir::Dirs | QDir::NoDotAndDotDot);
+	QFileInfoList files = dir.entryInfoList(QDir::Files);
+
+	foreach(QFileInfo fi, dirs) {
+		qInfo() << fi.fileName();
+	}
+
+	foreach(QFileInfo fi, files) {
+		qInfo() << " ..Name: " << fi.fileName();
+		qInfo() << " ...Size: " << fi.size();
+		qInfo() << " ...Created: " << fi.birthTime();
+		qInfo() << " ...Modified: " << fi.lastModified();
+	}
+
+	foreach(QFileInfo fi, dirs) {
+		list(fi.absoluteFilePath());
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	QCoreApplication a(argc, argv);
+
+	list(QDir::tempPath());
+
+	return a.exec();
+}
